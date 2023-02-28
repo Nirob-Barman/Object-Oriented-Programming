e82b738a59e28d2d94b65dc95e8b406f7b2c7db8
Nirob Barman <niroborin111@gmail.com>
2023-02-28 23:50:40 +0600

    chapter 5 modified

---
+++ Chapter-5-Classes-and-Objects/009-a-sample-program-to-introduce-returning-objects.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class complexNumber /// x + yj form
+{
+    float x; /// real part
+    float y; /// imaginary part
+public:
+    void input(float real, float imaginary)
+    {
+        x = real;
+        y = imaginary;
+    }
+    friend complexNumber sum(complexNumber, complexNumber);
+    void showComplexNumber(complexNumber c);
+};
+
+complexNumber sum(complexNumber one, complexNumber two)
+{
+    complexNumber add;
+    add.x = one.x + two.x;
+    add.y = one.y + two.y;
+    return add;
+}
+void complexNumber::showComplexNumber(complexNumber c)
+{
+    cout << c.x << " + " << c.y << "j" << endl;
+}
+
+int main()
+{
+    complexNumber A, B, C;
+    A.input(3.1, 5.65);
+    B.input(2.75, 1.2);
+
+    C = sum(A, B);
+    cout << "A = ";
+    A.showComplexNumber(A);
+    cout << "B = ";
+    B.showComplexNumber(B);
+    cout << "C = ";
+    C.showComplexNumber(C);
+}
