d6ccfbe3ac0e70d6fa87402cf2883edf5d90b9b4
rsnirob <niroborin111@gmail.com>
2023-03-17 13:39:19 +0600

    a c++ program to overload unary operators using member operator function

---
+++ Chapter-7-Operator-Overloading/001 a c++ program to overload unary operators using member operator function.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class item
+{
+    int a,b;
+public:
+    void getData(int x,int y);
+    void putData();
+    void operator -();  ///overload unary minus
+};
+
+void item::getData(int x,int y)
+{
+    a = x;
+    b = y;
+}
+void item::putData()
+{
+    cout<<a<<endl<<b<<endl;
+}
+void item::operator-()
+{
+    a=-a;
+    b=-b;
+}
+
+int main()
+{
+    item object;
+    object.getData(10, -20);
+    cout<<"without Overloading"<<endl;
+    object.putData();
+    -object;
+    cout<<"After Overloading"<<endl;
+    object.putData();
+}
