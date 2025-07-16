e437fc2506c8fe2374302d634834ddc8409f4bc1
Nirob-Barman <niroborin111@gmail.com>
2025-07-16 08:36:16 +0600

    a sample program to introduce dynamic initialization of objects

--- Chapter-6-Constructor-and-Destructor-Functions/004-a-sample-program-to-introduce-dynamic-initialization-of-objects.cpp
+++ Chapter-6-Constructor-and-Destructor-Functions/004-a-sample-program-to-introduce-dynamic-initialization-of-objects.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
+class Rectangle
+{
+    int length, breadth;
+
+public:
+    // Constructor with dynamic initialization
+    Rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    int area()
+    {
+        return length * breadth;
+    }
+};
+
 int main()
 {
-
+    Rectangle rect(3, 4);
+    cout << "Area of rectangle = " << rect.area() << endl;
     return 0;
 }
