5568bc543f3b9b507ae750071010fda238e024e7
Nirob-Barman <niroborin111@gmail.com>
2025-07-18 08:10:17 +0600

    a cpp program to overload unary operators using member operator function

--- Chapter-7-Operator-Overloading/001-a-sample-program-to-overload-unary-opeations-using-member-operator-function.cpp
+++ Chapter-7-Operator-Overloading/001-a-sample-program-to-overload-unary-opeations-using-member-operator-function.cpp
@@ -7,27 +7,43 @@ class Item
     int a, b;
 
 public:
-    void setData(int x, int y);
-    void displayData();
-    void operator-(); // overload unary minus
+    // void setData(int x, int y);
+    // void displayData();
+    // void operator-(); // overload unary minus
+    void setData(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void displayData()
+    {
+        cout << a << " " << b << endl;
+    }
+
+    void operator-() // overload unary minus
+    {
+        a = -a;
+        b = -b;
+    }
 };
 
-void Item::setData(int x, int y)
-{
-    a = x;
-    b = y;
-}
+// void Item::setData(int x, int y)
+// {
+//     a = x;
+//     b = y;
+// }
 
-void Item::displayData()
-{
-    cout << a << " " << b << endl;
-}
+// void Item::displayData()
+// {
+//     cout << a << " " << b << endl;
+// }
 
-void Item::operator-()
-{
-    a = -a;
-    b = -b;
-}
+// void Item::operator-()
+// {
+//     a = -a;
+//     b = -b;
+// }
 
 int main()
 {
@@ -42,3 +58,8 @@ int main()
 
     return 0;
 }
+
+// Before overloading
+// 10 -20
+// After overloading
+// -10 20
