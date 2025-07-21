003a73ea766970a923962c2ff23aff4f701d97b2
Nirob-Barman <niroborin111@gmail.com>
2025-07-21 08:07:57 +0600

    a c++ program to introduce nesting of member function

--- Chapter-5-Classes-and-Objects/003-a-c++-program-to-introduce-nesting-of-member-function.cpp
+++ Chapter-5-Classes-and-Objects/003-a-c++-program-to-introduce-nesting-of-member-function.cpp
@@ -8,29 +8,49 @@ class setFunction
     int m, n;
 
 public:
-    void input();
-    void display();
-    int largest();
+    // void input();
+    // void display();
+    // int largest();
+
+    // Function to find the largest number
+    int largest()
+    {
+        if (m >= n)
+            return m;
+        else
+            return n;
+    }
+    // Function to get input
+    void input()
+    {
+        cout << "Input values of m and n is : ";
+        cin >> m >> n;
+    }
+    // Function to display the largest number
+    void display()
+    {
+        cout << "Largest value = " << largest() << endl;
+    }
 };
 
-int setFunction::largest()
-{
-    if (m >= n)
-        return m;
-    else
-        return n;
-}
+// int setFunction::largest()
+// {
+//     if (m >= n)
+//         return m;
+//     else
+//         return n;
+// }
 
-void setFunction::input()
-{
-    cout << "Input values of m and n is : ";
-    cin >> m >> n;
-}
+// void setFunction::input()
+// {
+//     cout << "Input values of m and n is : ";
+//     cin >> m >> n;
+// }
 
-void setFunction::display()
-{
-    cout << "Largest value = " << largest() << endl;
-}
+// void setFunction::display()
+// {
+//     cout << "Largest value = " << largest() << endl;
+// }
 
 int main()
 {
